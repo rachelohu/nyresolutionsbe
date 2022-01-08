3ea3e2623466d3b2910d372131c22e31b7ac3c09
rachelohu <rachelohu5@gmail.com>
2022-01-08 05:45:16 -0500

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_203325) do
+ActiveRecord::Schema.define(version: 2022_01_07_052425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,5 +21,5 @@ ActiveRecord::Schema.define(version: 2022_01_06_203325) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
+  
 end
